d26909f7f04b82ebb1107dd3a6e4a4c26562d1f3
paulina koch <pjassakoch@gmail.com>
2024-11-17 20:10:43 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import React from 'react';
+import Nav from './components/Nav'
+import Card from './components/Card'
+
+import './App.css'
+
+function App() {
+ 
+  return (
+    
+      <div className='.container'>
+        <Nav/>
+        <Card/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Data.jsx
@@ -0,0 +1,21 @@
+export default [
+    {
+        id: 1,
+        title: "Attend a Banana Ball Game",
+        image: "baseball.jpg",
+        description: "The Savannah Bananas make a baseball game not just about the score but about having the most fun possible. There's dance-offs, dacing umpires, trick plays and costumes. It's baseball mixed with a dance party and comedy show.",
+        
+    }, 
+    {
+        id: 2,
+        title: "Take Samba Lessons",
+        image: "samba.jpg",
+        description: "Brazilian samba is a lively dance and music style from Brazil that has a fast beat and rhythm.",
+    },
+    {
+        id: 3,
+        title: "Girls Trip to Nashville",
+        image: "nashville.jpg",
+        description: "A girls' trip to Nashville would include hitting up the famous honky-tonk bars on Broadway, where we'd go line dancing and enjoy live country music.",
+    }             
+]
